101e06b218235ae7b9beeaf65fac4ee28848f1be
Allo3 <allo.louchet@gmail.com>
2023-09-04 22:51:39 +0200

    fix/add data to slug for fix call api

--- src/services/slug.js
+++ src/services/slug.js
@@ -3,7 +3,7 @@ import axios from "axios"
 const API_URL = process.env.REACT_APP_STRAPI_URL;
 export async function fetchProjectBySlug(slug) {
     try {
-        return await axios.get(`${API_URL}/projects?populate\\[media\\]=media&populate\\[screenMedia\\]=screenMedia&populate\\[tags\\]\\[populate\\]\\[logo\\]=tags&filters\\[slug\\]\\[$eq\\]=${slug}`)
+        return await axios.get(`${API_URL}/projects?populate[media][data]=media&populate[screenMedia][data]=screenMedia&populate[tags][populate][logo][data]=tags&filters[slug][$eq]=${slug}`)
     } catch (e) {
         throw new Error(`Erreur lors de la récupération du projet ${slug} : ${e.message}`);
     }

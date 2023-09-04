08026ca91e85b9ecca2c2f5e2f17a0e10e398746
Allo3 <allo.louchet@gmail.com>
2023-09-04 22:47:57 +0200

    fix/url-call-api

--- src/services/project.js
+++ src/services/project.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const API_URL = process.env.REACT_APP_STRAPI_URL;
 export async function fetchProjects() {
     try {
-        return await axios.get(`${API_URL}/projects?populate[media]=media&populate[tags][populate][logo]=tags`);
+        return await axios.get(`${API_URL}/projects?populate\\[media\\]=media&populate\\[tags\\]\\[populate\\]\\[logo\\]=tags`);
     } catch (error) {
         throw new Error("Erreur lors de la récupération des projets :", error);
     }
